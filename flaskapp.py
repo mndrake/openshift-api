af9063ece49b4e647c22682224da72571b53a418
David Carlson <dacarlson@mac.com>
2017-06-17 22:15:33 -0500

    fixed app

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_restplus import Api
+from flask_restplus import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@api.route('/')
-def hello_world():
-    return 'Hello World!'
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=3000)

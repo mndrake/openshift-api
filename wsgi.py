2a2246b87ae6820c7a5ad5e0d16354afc46989f4
David Carlson <dacarlson@mac.com>
2017-06-17 21:37:01 -0500

    fixed app

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,28 @@
-from flask import Flask
+#!/usr/bin/python
+import os
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR', '.'), 'virtenv')
 
 
+#virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from flaskapp import app as application
+
+#
+# Below for testing only
+#
 if __name__ == '__main__':
-    app.run()
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    #httpd.handle_request()
+    httpd.serve_forever()
